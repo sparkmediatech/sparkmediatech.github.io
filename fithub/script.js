5c0e5d5be95cd274064db82d676fff0feed76de3
sparkmediatech <79198117+sparkmediatech@users.noreply.github.com>
2021-04-28 06:36:22 -0700

    Update script.js

--- fithub/script.js
+++ fithub/script.js
@@ -475,14 +475,14 @@ var mobile = window.matchMedia("(max-width: 500px)")
   /* Mobile*/
   var m = window.matchMedia("(max-width: 400px) and (min-width: 320px)")
   if(m.matches){
-    slide1box1.style.top = "20px"
+    slide1box1.style.top = "8px"
 
     
 
     function mySection1Slide1Out(){
         slide1.style.left = "-100vw";
         slide1box1.style.opacity = "0";
-        slide1box1.style.top = "-2px"
+        slide1box1.style.top = "-1px"
         slide1BTN.style.opacity = "0";
         delayInterval = setTimeout(mySection1Slide2, firstInterval);
       }
